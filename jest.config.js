407f900eefcd2c2072b42c6e3d109ad244179794
PJ - Data Analytics 2024.1 <3668822024.1@senacrjedu.local>
2024-04-11 09:22:08 -0300

    tst

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config({ path: '.env.test' });
 
 export default {
-  // ...ou module.exports se preferir
+  // ...ou module.exports se pref
   testEnvironment: 'node',
   transform: {
     '^.+\\.js$': 'babel-jest',
